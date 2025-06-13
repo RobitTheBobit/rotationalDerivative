0fa173840e54015a366f4da1f2b18e2422461924
RobitTheBobit <142263594+RobitTheBobit@users.noreply.github.com>
2025-06-13 11:40:07 -0400

    Add files via upload

---
+++ rotationalDerivative.m
@@ -0,0 +1,10 @@
+theta = input("Please enter theta (derivative value): ");
+c = input("Please enter c value: ");
+% temporary x line until I get my function outputting normally
+x = input("Please enter a specific x value: ")
+
+"The thetath Rotational Derivative is Equal to..."
+result = c^theta * sin(c*x + theta * (pi / 2))
+
+
+
