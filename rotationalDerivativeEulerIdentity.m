3780050344705d0d0b6f0efae20875bc4c07e659
RobitTheBobit <142263594+RobitTheBobit@users.noreply.github.com>
2025-06-15 13:06:47 -0400

    Add files via upload

---
+++ rotationalDerivativeEulerIdentity.m
@@ -0,0 +1,10 @@
+theta = input("Please enter theta (derivative value): ");
+b = input("Please enter b value: ");
+% temporary x line until I get my function outputting normally
+x = input("Please enter a specific x value: ");
+
+"The thetath Rotational Derivative is Equal to..."
+result = (i*b)^theta * exp(i*b*x)
+
+
+
